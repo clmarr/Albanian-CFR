5c8020b211fe6fd25f9a385cf431c95c9b5af98a
clmarr <marr.54@buckeyemail.osu.edu>
2025-09-23 14:45:40 -0400

    lex work

---
+++ merge_lex.py
@@ -0,0 +1,10 @@
+
+
+OUTPUT = "gjALex.txt"
+STAGES = ["Proto-Indo-European", "Proto-Balkan-Indo-European",
+          "Archaic Proto-Albanian", "Classical Proto-Albanian",
+          "Middle Proto-Albanian I", "Middle Proto-Albanian II",
+          "Late Proto-Albanian I", "Late Proto-Albanian II", "Proto-Tosk",
+          "Old North Tosk", "Modern Standard Albanian"]
+
+INPUT_FILES = []

3405ffca92c4ef7da6cbb2e18e0c423bd9fd7647
Clayton Marr <marr.54@buckeyemail.osu.edu>
2025-10-13 11:03:35 -0400

    change UTILS.col_check to return tuple of lists

--- UTILS.py
+++ UTILS.py
@@ -1,15 +1,22 @@
-
 HEADER_FLAG = "="
 HEADER_DELIM = ","
 CMT_FLAG = "$"
 LEX_DELIM = " , "
+ABSENT_ETYM = "..."
+UNATTD_ETYM = ">*"
+STAGES = ["Proto-Indo-European", "Proto-Balkan-Indo-European",
+          "Archaic Proto-Albanian", "Classical Proto-Albanian",
+          "Middle Proto-Albanian I", "Middle Proto-Albanian II",
+          "Late Proto-Albanian I", "Late Proto-Albanian II", "Proto-Tosk",
+          "Old North Tosk", "Modern Standard Albanian"]
 
-def col_check(file_loc, numcols = False):
+
+def col_check(file_loc, numcols=False):
     f = open(file_loc, encoding="utf-8")
     lines = [line for line in f.readlines() if len(line.strip()) > 0]
     f.close()
 
-    startloc = int(lines[0][0] == HEADER_FLAG) #1 if true else 0
+    startloc = int(lines[0][0] == HEADER_FLAG)  # 1 if true else 0
     ncol = numcols if numcols else len(lines[startloc].split(CMT_FLAG)[0].split([HEADER_DELIM, LEX_DELIM][startloc]))
 
     error_rows = []
@@ -18,7 +25,7 @@ def col_check(file_loc, numcols = False):
     for li in range(startloc, len(lines)):
         commented = CMT_FLAG in lines[li]
         content = lines[li] if not commented else lines[li].split(CMT_FLAG)[0]
-        if len(content.strip()) == 0 :
+        if len(content.strip()) == 0:
             continue
         cols_here = len(content.split(LEX_DELIM))
         if cols_here != ncol:
@@ -27,16 +34,4 @@ def col_check(file_loc, numcols = False):
             if len(lines[li].split(CMT_FLAG)[1].split(LEX_DELIM)) > 1:
                 rows_w_commented_delim += [li]
 
-    if len(error_rows) > 0:
-        print("Rows with errant sizes: " + ", ".join([str(ri) for ri in error_rows]))
-    if len(rows_w_commented_delim) > 0:
-        print("Rows with column delimiters in comments: "+ ", ".join([str(ri) for ri in rows_w_commented_delim]))
-
-    if len(error_rows) == 0:
-        print("no errant rows!")
-    else:
-        print("error rows:")
-        if len(error_rows) > 0:
-            for eri in error_rows:
-                print(str(eri)+": "+lines[eri][:lines[eri].index(CMT_FLAG)])
-
+    return error_rows, rows_w_commented_delim
--- column_check.py
+++ column_check.py
@@ -8,6 +8,19 @@ parser.add_argument("-f","--file", default="ALLAPS.txt")
 parser.add_argument("-n","--numcols", default=False)
 args = parser.parse_args()
 
-UTILS.col_check(args.file, args.numcols)
 
+error_rows, rows_w_commented_delim = UTILS.col_check(args.file, args.numcols)
+
+if len(error_rows) > 0:
+    print("Rows with errant sizes: " + ", ".join([str(ri) for ri in error_rows]))
+if len(rows_w_commented_delim) > 0:
+    print("Rows with column delimiters in comments: " + ", ".join([str(ri) for ri in rows_w_commented_delim]))
+
+if len(error_rows) == 0:
+    print("no errant rows!")
+else:
+    print("error rows:")
+    if len(error_rows) > 0:
+        for eri in error_rows:
+            print(str(eri) + ": " + lines[eri][:lines[eri].index(CMT_FLAG)])
 

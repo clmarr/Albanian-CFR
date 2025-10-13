497d00ec07286234a92e62c63cad1cb9e3295d40
Clayton Marr <marr.54@buckeyemail.osu.edu>
2025-10-13 11:21:12 -0400

    col chekc report established as function in UTILS too

--- UTILS.py
+++ UTILS.py
@@ -35,3 +35,42 @@ def col_check(file_loc, numcols=False):
                 rows_w_commented_delim += [li]
 
     return error_rows, rows_w_commented_delim
+
+
+def col_check_report (file, ncols = False):
+    error_rows, rows_w_commented_delim = col_check(file, ncols)
+
+    if len(error_rows) > 0:
+        print("Rows with errant sizes: " + ", ".join([str(ri) for ri in error_rows]))
+    if len(rows_w_commented_delim) > 0:
+        print("Rows with column delimiters in comments: " + ", ".join([str(ri) for ri in rows_w_commented_delim]))
+
+    if len(error_rows) == 0:
+        print("no errant rows!")
+    else:
+        print("error rows:")
+        f = open(file, encoding="utf-8")
+        lines = [line for line in f.readlines() if len(line.strip()) > 0]
+        f.close()
+        for eri in error_rows:
+            print(str(eri) + ": " + lines[eri][:lines[eri].index(UTILS.CMT_FLAG)])
+
+
+# f file, c column number, m mutandum (thing being changed), r result
+def change_transcription(f,c,m,r):
+    # checking for column errors....
+    err_rows= col_check(f)[0]
+
+    if len(err_rows) > 0:
+        print("Warning: there are errant rows present...")
+        f = open(f, encoding="utf-8")
+        lines = [line for line in f.readlines() if len(line.strip()) > 0]
+        f.close()
+        for eri in err_rows:
+            print(str(eri) + ": " + lines[eri][:lines[eri].index(UTILS.CMT_FLAG)])
+        raise Exception("Tried to change a transcription in a file with inconsistent columns. Aborting for security purposes.")
+    else:
+        print("No inconsistency in column count among rows, thankfully.")
+
+
+
--- column_check.py
+++ column_check.py
@@ -8,21 +8,5 @@ parser.add_argument("-f","--file", default="ALLAPS.txt")
 parser.add_argument("-n","--numcols", default=False)
 args = parser.parse_args()
 
-
-error_rows, rows_w_commented_delim = UTILS.col_check(args.file, args.numcols)
-
-if len(error_rows) > 0:
-    print("Rows with errant sizes: " + ", ".join([str(ri) for ri in error_rows]))
-if len(rows_w_commented_delim) > 0:
-    print("Rows with column delimiters in comments: " + ", ".join([str(ri) for ri in rows_w_commented_delim]))
-
-if len(error_rows) == 0:
-    print("no errant rows!")
-else:
-    print("error rows:")
-    f = open(args.file, encoding="utf-8")
-    lines = [line for line in f.readlines() if len(line.strip()) > 0]
-    f.close()
-    for eri in error_rows:
-        print(str(eri) + ": " + lines[eri][:lines[eri].index(UTILS.CMT_FLAG)])
+UTILS.col_check_report(args.file, args.numcols)
 
